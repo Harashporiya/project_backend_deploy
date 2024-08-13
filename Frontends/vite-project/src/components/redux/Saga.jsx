6d3387a57c70aa452cb425b9784eb176fcf38c8f
Your Name <your_email@example.com>
2024-08-13 23:44:38 +0530

    movies

--- Frontends/vite-project/src/components/redux/Saga.jsx
+++ Frontends/vite-project/src/components/redux/Saga.jsx
@@ -1,6 +1,6 @@
 import { call, put, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
-import { DEPLOY_URL } from '../../../backend_API/API';
+import { DEPLOY_URL } from '../backend_API/API';
 import {
   signupUserSuccess,
   signupUserFailed,
